1b03f15f2033f35eeb2d2c7a9a03d47779143de4
mohammedoweis <mohammed.oweis.mohammed@gmail.com>
2022-12-25 23:58:32 +0200

    functin update-delete-select

--- dbms.sh
+++ dbms.sh
@@ -35,7 +35,8 @@ function CreateSingleLineBreak() {
 function RegexMatch() {
 
 	[[ $1 =~ ^[0-9\.\!\@\#\$\%\^\&\*\(\)\?\<\>\=\-\{\}\'\"\/\|\~\,\;\+\:\_]\w* || 
-						$1 =~ [[:space:]] ||
+						$1 == *" "* ||
+						$1 =~ [[:space:]]+ ||
 						$1 == *[\.\!\@\#\$\%\^\&\*\(\)\?\<\>\=\{\}\'\"\/\|\~\,\;\+\:]* ]]
 
 }
@@ -43,7 +44,7 @@ function RegexMatch() {
 function MainMenu() {
 
 	PS3='Enter Your Choice: '	
-	select choice in 'Create_Database' 'Select_Database' 'Show_Databases' 'Rename_Databse' 	'Drop_Database' 'Exit'
+	select choice in 'Create_Database' 'Select_Database' 'Show_Databases' 'Rename_Database' 'Drop_Database' 'Exit'
 		
 		do	
 	
@@ -53,7 +54,7 @@ function MainMenu() {
 				SelectDatabse
 			elif [[ $choice = 'Show_Databases' ]]; then
 				ShowDatabases
-			elif [[ $choice = 'Rename_Databse' ]]; then
+			elif [[ $choice = 'Rename_Database' ]]; then
 				RenameDatabase
 			elif [[ $choice = 'Drop_Database' ]]; then
 				DropDatabase
@@ -102,10 +103,10 @@ function SelectDatabse() {
 		cd Databases/${database_name^^}
 		 
 		CreateSingleLineBreak
-		DatabaseDetails
+		DatabaseDetailsMenu
 		#echo "You are now in $database_name database"
 	else
-		CreateDoubleLineBreak "$database_name database does not exist. We recommend to show 			database first to ba aware of existing databases"
+		CreateDoubleLineBreak "$database_name database does not exist. We recommend to show database first to ba aware of existing databases"
 		MainMenu
 	fi
 }
@@ -161,11 +162,11 @@ function DropDatabase() {
 
 
 
-function DatabaseDetails() {
+function DatabaseDetailsMenu() {
 
 
 	PS3='Please select a choice: '
-	select choice in 'Show_Existing_Tables' 'Create_New_Table' 'Show_Table' 'Drop_Table' 		'Rename_Table' 'Select_From_Table' 'Insert_Data_Into_Table' 'Delete_Data_From_Table' 		'Update_Table' 'Get_Back_To_Main_Menu' 'Exit'
+	select choice in 'Show_Existing_Tables' 'Create_New_Table' 'Drop_Table' 		'Rename_Table' 'Select_Data_From_Table' 'Insert_Data_Into_Table' 'Delete_Data_From_Table' 		'Update_Table' 'Get_Back_To_Main_Menu' 'Exit'
 	
 	do
 	
@@ -173,18 +174,19 @@ function DatabaseDetails() {
 			ShowExistingTables
 		elif [[ $choice = 'Create_New_Table' ]]; then
 			CreateNewTable
-		elif [[ $choice = 'Show_Table' ]]; then
-			ShowTable
+		elif [[ $choice = 'Select_Data_From_Table' ]]; then
+			CreateSingleLineBreak
+			SelectionMenu
 		elif [[ $choice = 'Drop_Table' ]]; then
 			DropTable
 		elif [[ $choice = 'Rename_Table' ]]; then
 			RenameTable
 		elif [[ $choice = 'Insert_Data_Into_Table' ]]; then
-			echo 'Data has been inserted'
+			InsertDataIntoTable
 		elif [[ $choice = 'Delete_Data_From_Table' ]]; then
-			echo 'Data has been deleted'
+			DeleteDataFromTable
 		elif [[ $choice = 'Update_Table' ]]; then
-			echo 'Table has been updated'
+			UpdateTable
 		elif [[ $choice = 'Get_Back_To_Main_Menu' ]]; then
 			CreateSingleLineBreak
 			cd ../..
@@ -203,30 +205,359 @@ function DatabaseDetails() {
 function CreateNewTable() {
 
 	read -p 'Enter name of the table: ' table_name
-	
-	
+		
 	if ! [[ -f $table_name ]]; then
 		if ! RegexMatch $table_name; then
-						
-			touch $table_name
-			CreateDoubleLineBreak 'The table has been created'
-			DatabaseDetails
-		
+			
+  		read -p 'Number of columns: ' columns_number
+  		
+			counter=1
+			field_sep="|"
+			record_sep="\n"
+			primary_key=""
+			metadata="Field"$field_sep"Type"$field_sep"key"
+			while [ $counter -le $columns_number ]
+				do
+					read -p "Name of Column No. $counter: " column_name
+					echo -e "Type of Column $counter: "
+					PS3='Enter type of field: '
+					select choice in "int" "str"
+					do
+						case $choice in
+						  int ) column_type="int";break;;
+						  str ) column_type="str";break;;
+						  * ) echo "Wrong Choice" ;;
+						esac
+					done
+					if [[ $primary_key == "" ]]; then
+						echo -e "Make it Primary Key? "
+						PS3='Make your choice: '
+						select choice in "yes" "no"
+						do
+						  case $choice in
+						    yes ) primary_key="PK";
+						    metadata+=$record_sep$column_name$field_sep$column_type$field_sep$primary_key
+						    break;;
+						    no )
+						    metadata+=$record_sep$column_name$field_sep$column_type$field_sep""
+						    break;;
+						    * ) echo "Wrong Choice" ;;
+						  esac
+						done
+					else
+						metadata+=$record_sep$column_name$field_sep$column_type$field_sep""
+					fi
+					if [[ $counter == $columns_number ]]; then
+						temp=$temp$column_name
+					else
+						temp=$temp$column_name$field_sep
+					fi
+					((counter++))
+				done
+			
+				touch .$table_name
+				echo  -e $metadata  >> .$table_name
+				touch $table_name
+				echo  -e $temp >> $table_name
+
+				CreateDoubleLineBreak 'The table has been created'
+				DatabaseDetailsMenu
+	
 		else
 			CreateDoubleLineBreak 'Name is NOT valid!. Rules:
 				1) Not starts with numbers
 				2) Not starts with special character
 				3) Not contains spaces
-				4) Not contain special characters except - or _
-			'
-			DatabaseDetails
+				4) Not contain special characters except - or _'
+			DatabaseDetailsMenu
 		fi
 	else
 		CreateDoubleLineBreak "This table already exists"
-		DatabaseDetails
+		DatabaseDetailsMenu
 	fi	
 		
 }
+
+
+function InsertDataIntoTable() {
+
+
+read -p 'Enter name of the table: ' table_name
+  if ! [[ -f $table_name ]]; then
+    
+		CreateDoubleLineBreak "This table does not exist"
+		DatabaseDetailsMenu
+		
+  fi
+	
+	columns_number=`awk 'END{print NR}' .$table_name`
+	field_sep="|"
+	record_sep="\n"
+	
+	for (( i = 2; i <= $columns_number; i++ ));
+		do
+			column_name=$(awk 'BEGIN{FS="|"}{ if(NR=='$i') print $1}' .$table_name)
+			column_type=$( awk 'BEGIN{FS="|"}{if(NR=='$i') print $2}' .$table_name)
+			column_key=$( awk 'BEGIN{FS="|"}{if(NR=='$i') print $3}' .$table_name)
+			
+			 #echo -e "$column_name ($column_type) = \c"
+       #read data
+       read -p "$column_name ($column_type) = " data
+
+			# Data Validation
+			if [[ $column_type == "int" ]]; then
+			  while ! [[ $data =~ ^[0-9]+$ ]];
+			  	do
+					  echo "invalid datatype!"
+					  #echo -e "$column_name ($column_type) = \c"
+      			#read data
+      			read -p "$column_name ($column_type) = " data
+				 done
+			fi
+
+			if [[ $column_type == "PK" ]]; then
+			  while true 
+			  	do
+					  if [[ $data =~ ^[`awk 'BEGIN{FS="|" ; ORS=" "}{if(NR != 1)print $(('$i'-1))}' $table_name`]$ ]]; then
+					    echo  "NOT a valid input for Primary Key that should be unique and not null!"
+					  else
+					    break;
+					  fi
+					  #echo -e "$column_name ($column_type) = \c"
+       			#read data
+       			read -p "$column_name ($column_type) = " data
+			  	done
+			fi
+
+			#Set row
+			if [[ $i == $columns_number ]]; then
+			  row=$row$data$record_sep
+			else
+			  row=$row$data$field_sep
+			fi
+		done
+		
+	echo -e $row"\c" >> $table_name
+	if [[ $? == 0 ]]
+  then
+    CreateDoubleLineBreak "Data Inserted Successfully"
+  else
+    CreateDoubleLineBreak "Error occured while inserting data into $tableName table"
+  fi
+
+	row=""
+	DatabaseDetailsMenu	
+		
+}
+
+
+
+function DeleteDataFromTable() {
+
+	read -p 'Enter name of the table: ' table_name
+	
+	if [[ -f $table_name ]]; then
+		read -p 'Enter field name: ' field_name
+
+		field_id=$(awk 'BEGIN{FS="|"}{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$field_name'") print i}}}' $table_name)
+		if [[ $field_id == "" ]]; then
+		  CreateDoubleLineBreak "Field NOT found in $table_name table"
+		  DatabaseDetailsMenu
+		else
+		  read -p 'Enter field value: ' field_value
+		  result=$(awk 'BEGIN{FS="|"}{if ($'$field_id'=="'$field_value'") print $'$field_id'}' $table_name)
+		  if [[ $result == "" ]]; then
+		    CreateDoubleLineBreak "Value NOT found"
+		    DatabaseDetailsMenu
+		  else
+		    NR=$(awk 'BEGIN{FS="|"}{if ($'$field_id'=="'$field_value'") print NR}' $table_name)
+		    sed -i ''$NR'd' $table_name 
+		    CreateDoubleLineBreak "Row has been deleted successfully"
+		    DatabaseDetailsMenu
+		  fi
+		fi
+		else
+			CreateDoubleLineBreak "This table does not exist"
+			DatabaseDetailsMenu
+		fi
+
+}
+
+
+
+function UpdateTable() {
+
+	read -p 'Enter name of the table: ' table_name
+	
+	if [[ -f $table_name ]]; then
+		read -p 'Enter record number: ' record_number
+	
+		if [[ $record_number =~ ^[1-9]+$ ]]; then
+		
+			rows_number=`awk '{print NR}' $table_name | tail -1` 
+			
+			if [[ $record_number -le $rows_number ]]; then
+				read -p 'Enter value you want to change: ' old_value
+				
+				
+				awk "NR==$record_number{print}" $table_name
+				
+				result=`awk "NR==$record_number{print}" $table_name | grep $old_value`
+				
+				if [[ $result == '' ]]; then
+					CreateDoubleLineBreak 'This value does not exist'
+					DatabaseDetailsMenu
+				else
+					
+					read -p 'Enter new value: ' new_value
+					sed -i ''$record_number's/'$old_value'/'$new_value'/' $table_name
+					
+					CreateDoubleLineBreak 'Tabel has been updated'
+					DatabaseDetailsMenu
+				fi
+				
+		else
+				CreateDoubleLineBreak 'Column number NOT found'
+				DatabaseDetailsMenu
+			fi
+		else
+			CreateDoubleLineBreak 'Not valid number'
+			DatabaseDetailsMenu
+		fi
+		
+	else
+		CreateDoubleLineBreak 'Table does not exist'
+		DatabaseDetailsMenu
+	fi
+		
+	
+
+}
+
+
+
+function SelectionMenu() {
+
+
+		PS3='Please select a choice: '
+		select choice in 'Select_All_Columns' 'Select_Specific_Column' 'Select_Specific_Record' 'Get_Back_To_DatabaseDetailsMenu' 'Get_Back_To_Main_Menu' 'Exit'
+		
+			do
+				
+				if [[ $choice == 'Select_All_Columns' ]]; then
+					SelectAllColumns
+				elif [[ $choice == 'Select_Specific_Column' ]]; then
+					SelectSpecificColumn
+				elif [[ $choice == 'Select_Specific_Record' ]]; then
+					SelectSpecificRecord
+				elif [[ $choice ==  'Get_Back_To_DatabaseDetailsMenu' ]]; then
+					CreateSingleLineBreak
+					DatabaseDetailsMenu
+				elif [[ $choice == 'Get_Back_To_Main_Menu' ]]; then
+					cd ../..
+					CreateSingleLineBreak
+					MainMenu
+				elif [[ $choice == 'Exit' ]]; then
+					exit
+				else
+					CreateDoubleLineBreak 'Wrong choice!'
+				fi
+					
+			done	
+	
+
+}
+
+
+function SelectAllColumns() {
+
+	read -p 'Enter table name: ' table_name
+	
+	if [[ -f $table_name ]]; then
+		
+	CreateSingleLineBreak
+	column -t -s '|' $table_name
+	CreateSingleLineBreak
+	SelectionMenu
+	
+	else
+		CreateDoubleLineBreak 'This table does not exist!'
+		SelectionMenu
+	fi
+
+}
+
+
+
+function SelectSpecificColumn() {
+
+	
+	read -p 'Entet name of the table: ' table_name
+	
+	if [[ -f $table_name ]]; then
+		read -p 'Enter column number: ' column_number
+		
+		if [[ $column_number =~ ^[1-9]+$ ]]; then
+		
+			fields_number=`awk -F'|' '{print NF; exit}' $table_name` 
+			
+			if [[ $column_number -le $fields_number ]]; then
+					CreateSingleLineBreak
+					awk 'BEGIN{FS="|"}{print $'$column_number'}' $table_name
+					CreateSingleLineBreak
+					SelectionMenu
+			else
+				CreateDoubleLineBreak 'Column number NOT found'
+				SelectionMenu
+			fi
+		else
+			CreateDoubleLineBreak 'Not valid number'
+			SelectionMenu
+		fi
+		
+	else
+		CreateDoubleLineBreak 'Table does not exist'
+		SelectionMenu
+	fi
+
+}
+
+
+function SelectSpecificRecord() {
+
+	read -p 'Entet name of the table: ' table_name
+	
+	if [[ -f $table_name ]]; then
+		read -p 'Enter record number: ' record_number
+		
+		if [[ $record_number =~ ^[1-9]+$ ]]; then
+		
+			rows_number=`awk '{print NR}' $table_name | tail -1` 
+			
+			if [[ $record_number -le $rows_number ]]; then
+					CreateSingleLineBreak
+					awk "NR==$record_number{print}" $table_name
+					CreateSingleLineBreak
+					SelectionMenu
+			else
+				CreateDoubleLineBreak 'Row number NOT found'
+				SelectionMenu
+			fi
+		else
+			CreateDoubleLineBreak 'Not valid number'
+			SelectionMenu
+		fi
+		
+	else
+		CreateDoubleLineBreak 'Table does not exist'
+		SelectionMenu
+	fi
+}
+
+
+
+
+
 
 
 function ShowExistingTables() {
@@ -234,29 +565,30 @@ function ShowExistingTables() {
 	if [[ $(ls -A) ]]; then
 		content=`ls`
 		CreateDoubleLineBreak $content 
-		DatabaseDetails
+		DatabaseDetailsMenu
 	else
 		CreateDoubleLineBreak 'No Tables Yet!'
+		DatabaseDetailsMenu
 	fi
 	
 }
 
 
 
-function ShowTable() {
+#function ShowTable() {
 
-	read -p 'Enter name of the table: ' table_name
-	
-	if [[ -f $table_name ]]; then
-		cat $table_name
-			CreateSingleLineBreak
-		DatabaseDetails
-	else
-		CreateDoubleLineBreak 'This table does not exists' 
-		DatabaseDetails
-	fi
-}
-
+#	read -p 'Enter name of the table: ' table_name
+#	
+#	if [[ -f $table_name ]]; then
+#		result=`cat -nE $table_name`
+#		CreateDoubleLineBreak $result
+#		DatabaseDetailsMenu
+#	else
+#		CreateDoubleLineBreak 'This table does not exists' 
+#		DatabaseDetailsMenu
+#	fi
+#} echo -e "Enter Table Name: \c"
+  
 
 
 function DropTable() {
@@ -265,11 +597,12 @@ function DropTable() {
 	
 	if [[ -f $table_name ]]; then
 		rm $table_name
+		rm .$table_name
 		CreateDoubleLineBreak 'The table has been dropped' 
-		DatabaseDetails
+		DatabaseDetailsMenu
 	else
 		CreateDoubleLineBreak 'This table does not exists' 
-		DatabaseDetails
+		DatabaseDetailsMenu
 	fi
 } 
 
@@ -282,8 +615,9 @@ function RenameTable() {
 		read -p 'Enter new name: ' new_name
 		if ! RegexMatch $new_name; then
 			mv $old_name $new_name
+			mv .old_name .$new_name
 			CreateDoubleLineBreak "The table has been renamed"
-			DatabaseDetails
+			DatabaseDetailsMenu
 		else
 			
 			CreateDoubleLineBreak 'Name is NOT valid!. Rules:
@@ -292,11 +626,11 @@ function RenameTable() {
 				3) Not contains spaces
 				4) Not contain special characters except - or _
 			'
-			DatabaseDetails
+			DatabaseDetailsMenu
 		fi
 	else
 		CreateDoubleLineBreak 'This table does not exists'	
-		DatabaseDetails
+		DatabaseDetailsMenu
 	fi	
 }
 
